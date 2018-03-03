6324f2d41fd70ecb81a04c08bd4b3e8c81148b44
idemnias <idemnias@gmail.com>
2018-03-03 03:56:41 +0100

    1 subida

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    // Para el movimiento
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+    private Rigidbody2D rigidbody2D;
+
+    //para la animacion
+    Animator anim;
+
+    //Para el salto
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 700f;
+    //doble salto
+    bool doubleJump = false;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        //para el salto
+        //primero detectar el suelo
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+
+        if (grounded)
+            doubleJump = false;
+
+        anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+
+
+        if (!grounded) return;
+        //para el movimiento derecha izquierda
+        float move = Input.GetAxis ("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+    }
+
+    void Update()
+    {
+        //detectar si esta en el suelo para saltar con el espacio
+        //doble salto
+        if ((grounded || !doubleJump) && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            rigidbody2D.AddForce(new Vector2(0, jumpForce));
+
+            if (!doubleJump && !grounded)
+                doubleJump = true;
+
+        }
+    }
+
+    //cambio de sentido
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
